86a8c6402cd4080077d7cafe6fe2ab1053870b82
DESKTOP-8BSDEKG\admin <hotman7025@naver.com>
2022-10-23 09:44:05 +0900

    2022.10.23

---
+++ web-3-202210/JavaStudy202210-haehyuk/src/main/java/j01_출력/PrintTest1.java
@@ -0,0 +1,14 @@
+package j01_출력;
+
+public class PrintTest1 {
+    public static void main(String[] args) {
+        /*
+        여러줄 주석
+         */
+        //문자 출력
+        System.out.print('A');
+        //문자열 출력
+
+        System.out.println("\n\"이름\" 적을 때");
+    }
+}
